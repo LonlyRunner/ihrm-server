0541991d8ac28e1ba664d2774fb13e245619be5c
Frank <3069226178@qq.com>
2026-04-16 21:58:28 +0800

    初始化项目

---
+++ src/test/java/com/ihrm/ihrm/IHrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihrm.ihrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IHrmApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
